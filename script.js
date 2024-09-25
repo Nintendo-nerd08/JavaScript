79d6c3b6b87fb5516c292948f36f4c43db9b602a
lloydkev000 <lloydkevin631@gmail.com>
2024-09-25 08:25:26 -0400

    Updated code from lesson.

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ function display(){
     element.style.display = "block"
     element.style.color ="red"
 }
+
+let a = true;
+let b = false;
+console.log("A is " + a);
+console.log("B is " + b);
+
+console.log("Not A is " + !a);
+console.log("Not B is " + !b);
+
+console.log("A and B is " + a && b);
+console.log("A or B is " + (a || b)); // (|| <--- symbol for or.)
+
+
+function test(x) {
+    let element = document.getElementById("result");
+    x = +x;
+
+    if(x === 1){ //relates two things combines them and gives you a result.
+        element.innerHTML = "You clicked Button 1"
+    } else{
+        element.innerHTML = "You clicked Button 2"
+    }
+
+}
